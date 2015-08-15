d9704603091d0895e131ed66b1292968c188766f
Bare7a <bare7a@gmail.com>
2015-08-15 23:37:19 +0300

    Anime

--- js/video/genfunctions.js
+++ js/video/genfunctions.js
@@ -2,7 +2,6 @@ function generate(event)
 {
     var title = document.getElementById("title").value;
     var video = document.getElementById("video").value.substr(document.getElementById("video").value.indexOf("=") + 1);
-
     var textarea = document.getElementById("output");
     textarea.value = "{\n";
     textarea.value+= '  "title":'+'"'+title+'",\n';
